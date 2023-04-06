22a836a7e978cade6c49b1da966a885f0833e0b3
Aidan Scannell <scannell.aidan@gmail.com>
2023-04-06 16:43:09 +0300

    initial commit

---
+++ src/cluster_train.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import hydra
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="main")
+def train(cfg: DictConfig):
+    import train
+
+    train.train(cfg)
+
+
+if __name__ == "__main__":
+    train()  # pyright: ignore

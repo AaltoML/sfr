5dbf7f646ce42d23f5a66c8c6cca239513824636
Aidan Scannell <scannell.aidan@gmail.com>
2023-04-14 23:28:57 +0300

    made dimensions work for mppi

--- src/agents/objectives.py
+++ src/agents/objectives.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import torch
 from src.custom_types import State
 from src.models import RewardModel, TransitionModel
-import torch
+
+from .ddpg import Actor, Critic
 
 
 def greedy(
@@ -18,20 +20,31 @@ def greedy(
         """Estimate value of a trajectory starting at state and executing given actions."""
         state = start_state
         G, discount = 0, 1
-        print("state.shape {}".format(start_state.shape))
-        print("actions.shape {}".format(actions.shape))
+        # print("state.shape {}".format(start_state.shape))
+        # print("actions.shape {}".format(actions.shape))
         for t in range(horizon):
-            next_state = transition_model.predict(state, actions[t])
-            reward = reward_model.predict(state, actions[t])
-            print("reward at t={}: ".format(t, reward))
+            # TODO this isn't integrating uncertainty
+            next_state_prediction = transition_model.predict(state, actions[t])
+            # print("next_state_prediction")
+            # print(next_state_prediction.state_mean.shape)
+            reward_prediction = reward_model.predict(state, actions[t])
+            # print("reward at t={}: ".format(t, reward_prediction.reward_mean.shape))
+            # print(reward_prediction.reward_mean.shape)
             # print("state")
             # print(state.shape)
-            G += discount * reward
+            G += discount * reward_prediction.reward_mean
+            # print("G {}".format(G.shape))
             discount *= gamma
-            state = next_state["state_mean"]
-        G += discount * torch.min(
-            *critic(state, actor(state, std).sample(clip=std_clip))
+            state = next_state_prediction.state_mean
+        # print("final state {}".format(state.shape))
+        G_final = discount * torch.min(
+            *critic(
+                state,
+                actor(state, std).sample(clip=std_clip)
+                # state[None, ...], actor(state[None, ...], std).sample(clip=std_clip)
+            )
         )
-        return G
+        # print("G_final {}".format(G_final.shape))
+        return G[..., None] + G_final
 
     return greedy_fn

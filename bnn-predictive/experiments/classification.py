b6ed8243c0c1d31c20b3e87f02f6fde9e69f6444
Ella <ella.tamir.96@gmail.com>
2023-04-25 14:09:50 +0300

    Modified triton scripts

--- bnn-predictive/experiments/classification.py
+++ bnn-predictive/experiments/classification.py
@@ -265,6 +265,7 @@ if __name__ == '__main__':
         torch.set_default_dtype(torch.double)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.set_default_device(device)
 
     ds_train = UCIClassificationDatasets(dataset, random_seed=seed, root=data_dir, stratify=True,
                                          train=True, double=double)

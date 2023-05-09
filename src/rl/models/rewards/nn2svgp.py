b8dd193e3f8570d9626da7e9fb739147770d7b87
Aidan Scannell <scannell.aidan@gmail.com>
2023-05-09 10:19:30 +0300

    made class

--- src/rl/models/rewards/nn2svgp.py
+++ src/rl/models/rewards/nn2svgp.py
@@ -14,74 +14,91 @@ import src
 from .base import RewardModel
 
 
-def init(
-    network: torch.nn.Module,
-    learning_rate: float = 1e-2,
-    num_iterations: int = 1000,
-    batch_size: int = 64,
-    # num_workers: int = 1,
-    num_inducing: int = 100,
-    delta: float = 0.0001,  # weight decay
-    sigma_noise: float = 1.0,
-    jitter: float = 1e-4,
-    wandb_loss_name: str = "Transition model loss",
-    early_stopper: EarlyStopper = None,
-    device: str = "cuda",
-) -> RewardModel:
-    if "cuda" in device:
-        network.cuda()
-    likelihood = src.nn2svgp.likelihoods.Gaussian(sigma_noise=sigma_noise)
-    prior = src.nn2svgp.priors.Gaussian(params=network.parameters, delta=delta)
-    ntksvgp = src.nn2svgp.NTKSVGP(
-        network=network,
-        prior=prior,
-        likelihood=likelihood,
-        output_dim=1,
-        num_inducing=num_inducing,
-        jitter=jitter,
-    )
+class NTKSVGPRewardModel(RewardModel):
+    def __init__(
+        self,
+        network: torch.nn.Module,
+        learning_rate: float = 1e-2,
+        num_iterations: int = 1000,
+        batch_size: int = 64,
+        # num_workers: int = 1,
+        num_inducing: int = 100,
+        delta: float = 0.0001,  # weight decay
+        sigma_noise: float = 1.0,
+        jitter: float = 1e-4,
+        wandb_loss_name: str = "Reward model loss",
+        early_stopper: EarlyStopper = None,
+        device: str = "cuda",
+    ):
+        if "cuda" in device:
+            network.cuda()
+        self.network = network
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.batch_size = batch_size
+        self.delta = delta
+        self.sigma_noise = sigma_noise
+        self.wandb_loss_name = wandb_loss_name
+        self.early_stopper = early_stopper
+        self.device = device
 
-    # loss_fn = torch.nn.MSELoss()
-    # print("trans device {}".format(device))
-    if "cuda" in device:
-        ntksvgp.cuda()
-        print("put reward ntksvgp on cuda")
+        likelihood = src.nn2svgp.likelihoods.Gaussian(sigma_noise=sigma_noise)
+        prior = src.nn2svgp.priors.Gaussian(params=network.parameters, delta=delta)
+        self.ntksvgp = src.nn2svgp.NTKSVGP(
+            network=network,
+            prior=prior,
+            likelihood=likelihood,
+            output_dim=1,
+            num_inducing=num_inducing,
+            jitter=jitter,
+            device=device,
+        )
 
-    def predict_fn(state: State, action: Action) -> RewardPrediction:
+        # loss_fn = torch.nn.MSELoss()
+        # print("trans device {}".format(device))
+        if "cuda" in device:
+            self.ntksvgp.cuda()
+            print("put reward ntksvgp on cuda")
+
+    def predict_fn(self, state: State, action: Action) -> RewardPrediction:
         state_action_input = torch.concat([state, action], -1)
-        reward_mean = ntksvgp.predict_mean(state_action_input)
+        # reward_mean = ntksvgp.predict_mean(state_action_input)
+        reward_mean, reward_var = self.ntksvgp.predict_f(state_action_input)
         # print("reward_mean {}".format(reward_mean.shape))
         # TODO use reward_var??
         # delta_state_mean, delta_state_var, noise_var = svgp_predict_fn(
         return RewardPrediction(
-            reward_mean=reward_mean[:, 0], reward_var=0, noise_var=0
+            reward_mean=reward_mean[:, 0],
+            reward_var=reward_var[:, 0],
+            # reward_var=0,
+            noise_var=0,
         )
 
-    def train_fn(replay_buffer: ReplayBuffer):
-        if early_stopper is not None:
-            early_stopper.reset()
-        network.train()
+    def train_fn(self, replay_buffer: ReplayBuffer):
+        if self.early_stopper is not None:
+            self.early_stopper.reset()
+        self.network.train()
         optimizer = torch.optim.Adam(
-            [{"params": network.parameters()}], lr=learning_rate
+            [{"params": self.network.parameters()}], lr=self.learning_rate
         )
-        for i in range(num_iterations):
-            samples = replay_buffer.sample(batch_size=batch_size)
+        for i in range(self.num_iterations):
+            samples = replay_buffer.sample(batch_size=self.batch_size)
             state_action_inputs = torch.concat(
                 [samples["state"], samples["action"]], -1
             )
 
-            loss = ntksvgp.loss(x=state_action_inputs, y=samples["reward"])
+            loss = self.ntksvgp.loss(x=state_action_inputs, y=samples["reward"])
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            if wandb_loss_name is not None:
-                wandb.log({wandb_loss_name: loss})
+            if self.wandb_loss_name is not None:
+                wandb.log({self.wandb_loss_name: loss})
 
             logger.info("Iteration : {} | Loss: {}".format(i, loss))
-            if early_stopper is not None:
-                stop_flag = early_stopper(loss)
+            if self.early_stopper is not None:
+                stop_flag = self.early_stopper(loss)
                 if stop_flag:
                     logger.info("Early stopping criteria met, stopping training")
                     logger.info("Breaking out loop")
@@ -91,10 +108,8 @@ def init(
         state_action_inputs = torch.concat([data["state"], data["action"]], -1)
         reward = data["reward"]
         # print("reward {}".format(reward.shape))
-        ntksvgp.set_data((state_action_inputs, reward))
+        self.ntksvgp.set_data((state_action_inputs, reward))
 
-    def update_fn(data_new):
+    def update_fn(self, data_new):
         # ntksvgp.set_data((x, y))
-        return ntksvgp.update(x=data_new[0], y=data_new[1])
-
-    return RewardModel(predict=predict_fn, train=train_fn, update=update_fn)
+        return self.ntksvgp.update(x=data_new[0], y=data_new[1])

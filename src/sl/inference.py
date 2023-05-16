ed272ba7e2b1f9634901ae26a826ce5b11e407bb
Riccardo Mereu <riccardo.mereu@aalto.fi>
2023-05-17 00:02:57 +0300

    bnn/glm

--- src/sl/inference.py
+++ src/sl/inference.py
@@ -269,7 +269,7 @@ def compute_metrics(sfr, gp_subset, ds_train, ds_test, cfg, checkpoint):
             pred_type="glm",
             n_samples=100,
             diagonal_output=False,
-            generator=cfg.random_seed,
+            # generator=cfg.random_seed,
         )
         gstar_te, yte = get_la_predictive(test_loader, la_pred, seeding=True)
         gstar_va, yva = get_la_predictive(val_loader, la_pred, seeding=True)
@@ -287,7 +287,7 @@ def compute_metrics(sfr, gp_subset, ds_train, ds_test, cfg, checkpoint):
             pred_type="nn",
             n_samples=100,
             diagonal_output=False,
-            generator=cfg.random_seed,
+            # generator=cfg.random_seed,
         )
         gstar_te, yte = get_la_predictive(test_loader, la_pred, seeding=True)
         gstar_va, yva = get_la_predictive(val_loader, la_pred, seeding=True)

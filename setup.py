7f08dd603882a0d07f67e52bb655cd8d4e66248e
Aidan Scannell <scannell.aidan@gmail.com>
2023-05-19 19:25:56 +0300

    added hamiltorch

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ extras_require = {
         "tikzplotlib==0.10.1",
         "tabulate==0.9.0",
         "gym[classic_control]==0.26.2",
+        "hamiltorch @ git+ssh://git@github.com/AdamCobb/hamiltorch",  # TODO check this works # for HMC banada fig
     ],
 }
 

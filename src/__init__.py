4cb51a2bd715dbad5236517a4d72621c17327751
Aidan Scannell <scannell.aidan@gmail.com>
2023-05-19 20:42:43 +0300

    updatd

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # from src.rl import *
-# from src.rl import agents, models
+from src.rl import agents, models
+
 # from src.sfr import *
+# import src.rl.agents.mppi
+# import src.rl.agents.ddpg
+# import src.rl.models
 
 # from src.sl import train
 import src.sl.train
-import src.sl.inference
-import src.sl.datasets
-import src.sl.networks
+
+# import src.sl.inference
+
+# import src.sl.datasets
+# import src.sl.networks
 
 # import src.nn2svgp.likelihoods
 

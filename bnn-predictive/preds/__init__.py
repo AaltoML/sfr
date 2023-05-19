fe7cdc697473135bb1036a3d70382fc7dfb580f6
Aidan Scannell <scannell.aidan@gmail.com>
2023-05-19 21:56:18 +0300

    removed printing

--- bnn-predictive/preds/__init__.py
+++ bnn-predictive/preds/__init__.py
@@ -3,10 +3,3 @@ import os
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT = "/".join(PACKAGE_DIR.split("/")[:-1])
 DATA_DIR = ROOT + "/data"
-
-# PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
-print("PACKAGE_DIR {}".format(PACKAGE_DIR))
-# ROOT = "/".join(PACKAGE_DIR.split("/")[:-1])
-print("ROOT {}".format(ROOT))
-# DATA_DIR = os.path.join(ROOT, "bnn-predictive/data")
-print("DATA_DIR {}".format(DATA_DIR))

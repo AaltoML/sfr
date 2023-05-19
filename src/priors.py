aafef21051cc6bb5876b3005475ad4a3039e1f0d
Aidan Scannell <scannell.aidan@gmail.com>
2023-05-19 21:45:32 +0300

    added not implemented error

--- src/priors.py
+++ src/priors.py
@@ -2,9 +2,6 @@
 import torch
 import torch.nn as nn
 
-from typing import Union, Optional
-from src.custom_types import FuncData, FuncMean, FuncVar, OutputData
-
 
 class Prior(nn.Module):
     def __init__(self, params: nn.Parameter):
@@ -24,15 +21,12 @@ class Gaussian(Prior):
         self.delta = delta
 
     def log_prob(self):
-        # TODO is this right? Should it be delta**2?
+        raise NotImplementedError
         return torch.distributions.Normal(
-            torce.ones_like(self.params), self.delta**2 * torch.ones_like(self.params)
+            torch.ones_like(self.params), self.delta * torch.ones_like(self.params)
         ).log_prob(self.params)
 
     def nn_loss(self):
-        # print("PARAMS: {}".format(self.params))
-        # for param in self.params():
-        # print("param:m {}".format(param))
         squared_params = torch.cat(
             [torch.square(param.view(-1)) for param in self.params()]
         )

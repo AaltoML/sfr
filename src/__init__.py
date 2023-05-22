27e5001dcc678cd5228f779d4e38fbedc7bfa811
Aidan Scannell <scannell.aidan@gmail.com>
2023-05-22 13:26:01 +0300

    neatened

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,3 @@
 #!/usr/bin/env python3
-# from src.rl import *
-from src.rl import agents, models
-
-# from src.sfr import *
-# import src.rl.agents.mppi
-# import src.rl.agents.ddpg
-# import src.rl.models
-
-# from src.sl import train
-import src.sl.train
-
-# import src.sl.inference
-
-# import src.sl.datasets
-# import src.sl.networks
-
-# import src.nn2svgp.likelihoods
-
-# from .priors import *
-# from .likelihoods import *
-from .sfr import SFR, NTKSVGP
+from .sfr import SFR
 from .nn2gp import NN2GPSubset

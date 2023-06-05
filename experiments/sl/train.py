ff2e1e8cafd2539bd7ee4e24075070ead83946f3
Aidan Scannell <scannell.aidan@gmail.com>
2023-06-05 10:55:53 +0300

    moved bnn-predictive stuff back and import

--- experiments/sl/train.py
+++ experiments/sl/train.py
@@ -8,153 +8,32 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 import hydra
-import numpy as np
 import omegaconf
-import src
 import torch
 import wandb
-from experiments.sl.bnn_predictive.preds.datasets import CIFAR10, FMNIST, MNIST
-from experiments.sl.bnn_predictive.preds.models import CIFAR10Net, CIFAR100Net, MLPS
-from experiments.sl.inference import compute_metrics
+from experiments.sl.bnn_predictive.experiments.scripts.imgclassification import (
+    get_dataset,
+    get_model,
+)
+from experiments.sl.new_inference import compute_metrics, evaluate
+from experiments.sl.utils import set_seed_everywhere
 from omegaconf import DictConfig
-from src.likelihoods import BernoulliLh, CategoricalLh
 from torch.utils.data import DataLoader
-from torchvision.datasets import VisionDataset
 from tqdm import tqdm
 
 
-PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
-ROOT = "/".join(PACKAGE_DIR.split("/")[:-2])
-DATA_DIR = os.path.join(ROOT, "bnn-predictive/data")
+@torch.no_grad()
+def predict_probs(
+    dataloader: DataLoader, network: torch.nn.Module, device: str = "cpu"
+):
+    py = []
+    for x, _ in dataloader:
+        py.append(torch.softmax(network(x.to(device)), dim=-1))
+
+    return torch.cat(py).cpu().numpy()
 
 
-def set_seed_everywhere(random_seed):
-    random.seed(random_seed)
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed(random_seed)
-    torch.manual_seed(random_seed)
-    np.random.seed(random_seed)
-    random.seed(random_seed)
-
-
-class QuickDS(VisionDataset):
-    def __init__(self, ds, device):
-        self.D = [
-            (ds[i][0].to(device), torch.tensor(ds[i][1]).to(device))
-            for i in range(len(ds))
-        ]
-        self.K = ds.K
-        self.channels = ds.channels
-        self.pixels = ds.pixels
-
-    def __getitem__(self, index):
-        return self.D[index]
-
-    def __len__(self):
-        return len(self.D)
-
-
-def get_dataset(dataset, double, dir, cfg, device=None):
-    if dataset == "MNIST":
-        # Download training data from open datasets.
-        ds_train = MNIST(train=True, double=double, root=dir)
-        ds_test = MNIST(train=False, double=double, root=dir)
-    elif dataset == "FMNIST":
-        ds_train = FMNIST(train=True, double=double, root=dir)
-        ds_test = FMNIST(train=False, double=double, root=dir)
-    elif dataset == "CIFAR10":
-        ds_train = CIFAR10(train=True, double=double, root=dir)
-        ds_test = CIFAR10(train=False, double=double, root=dir)
-    else:
-        raise ValueError("Invalid dataset argument")
-    if device is not None:
-        if cfg.debug:
-            ds_train.data = ds_train.data[:500]
-            ds_train.targets = ds_train.targets[:500]
-            ds_test.data = ds_test.data[:500]
-            ds_test.targets = ds_test.targets[:500]
-        return QuickDS(ds_train, device), QuickDS(ds_test, device)
-    else:
-        return ds_train, ds_test
-
-
-def get_model(model_name, ds_train):
-    if model_name == "MLP":
-        input_size = ds_train.pixels**2 * ds_train.channels
-        hidden_sizes = [1024, 512, 256, 128]
-        output_size = ds_train.K
-        return MLPS(input_size, hidden_sizes, output_size, "tanh", flatten=True)
-    elif model_name == "SmallMLP":
-        input_size = ds_train.pixels**2 * ds_train.channels
-        hidden_sizes = [128, 128]
-        output_size = ds_train.K
-        return MLPS(input_size, hidden_sizes, output_size, "tanh", flatten=True)
-    elif model_name == "CNN":
-        return CIFAR10Net(ds_train.channels, ds_train.K, use_tanh=True)
-    elif model_name == "AllCNN":
-        return CIFAR100Net(ds_train.channels, ds_train.K)
-    else:
-        raise ValueError("Invalid model name")
-
-
-def evaluate(model, data_loader, criterion, device):
-    likelihood = src.likelihoods.CategoricalLh()
-    model.eval()
-    loss, acc, nll = 0, 0, 0
-    with torch.no_grad():
-        for X, y in data_loader:
-            X, y = X.to(device), y.to(device)
-            fs = model(X)
-            acc += (torch.argmax(fs, dim=-1) == y).sum().cpu().float().item()
-            loss += criterion(fs, y).item()
-            nll += -likelihood.log_prob(f=fs, y=y).sum().item()
-    return (
-        loss / len(data_loader.dataset),
-        acc / len(data_loader.dataset),
-        nll / len(data_loader.dataset),
-    )
-
-
-
-
-def nll_cls(p, y, likelihood):
-    """Avg. Negative log likelihood for classification"""
-    if type(likelihood) is BernoulliLh:
-        p_dist = Bernoulli(probs=p)
-        return -p_dist.log_prob(y).mean().item()
-    elif type(likelihood) is CategoricalLh:
-        p_dist = Categorical(probs=p)
-        return -p_dist.log_prob(y).mean().item()
-    else:
-        raise ValueError("Only Bernoulli and Categorical likelihood.")
-
-
-def ece(probs, labels, likelihood=None, bins=10):
-    # source: https://github.com/gpleiss/temperature_scaling/blob/master/temperature_scaling.py
-    if type(likelihood) is BernoulliLh:
-        probs = torch.stack([1 - probs, probs]).t()
-    bin_boundaries = torch.linspace(0, 1, bins + 1)
-    bin_lowers = bin_boundaries[:-1]
-    bin_uppers = bin_boundaries[1:]
-
-    confidences, predictions = torch.max(probs, 1)
-    accuracies = predictions.eq(labels.long())
-
-    ece = torch.zeros(1, device=probs.device)
-    for bin_lower, bin_upper in zip(bin_lowers, bin_uppers):
-        # Calculated |confidence - accuracy| in each bin
-        in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
-        prop_in_bin = in_bin.float().mean()
-        if prop_in_bin.item() > 0:
-            accuracy_in_bin = accuracies[in_bin].float().mean()
-            avg_confidence_in_bin = confidences[in_bin].mean()
-            ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
-
-    return ece.item()
-
-
-@hydra.main(version_base="1.3", config_path="./configs", config_name="main")
+@hydra.main(version_base="1.3", config_path="./configs", config_name="train")
 def train(cfg: DictConfig):
     try:  # Make experiment reproducible
         set_seed_everywhere(cfg.random_seed)
@@ -175,19 +54,21 @@ def train(cfg: DictConfig):
     cfg.device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using device: {}".format(cfg.device))
 
-
     ds_train, ds_test = get_dataset(
-        dataset=cfg.dataset, double=cfg.double, dir="./", device=cfg.device, cfg=cfg
+        dataset=cfg.dataset,
+        double=cfg.double,
+        dir="./",
+        device=cfg.device,
+        debug=cfg.debug,
     )
 
-    n_classes = 10
+    n_classes = ds_train.K
     print("n_classes {}".format(n_classes))
     cfg.output_dim = n_classes
 
     network = get_model(model_name=cfg.model_name, ds_train=ds_train)
     network = network.to(cfg.device)
-    prior = hydra.utils.instantiate(cfg.prior, params=network.parameters)
-    sfr = hydra.utils.instantiate(cfg.sfr, prior=prior, network=network)
+    sfr = hydra.utils.instantiate(cfg.sfr, model=network)
 
     if cfg.wandb.use_wandb:  # Initialise WandB
         run = wandb.init(
@@ -201,9 +82,13 @@ def train(cfg: DictConfig):
         )
 
     train_loader = DataLoader(ds_train, batch_size=cfg.batch_size, shuffle=True)
+    test_loader = DataLoader(ds_test, batch_size=cfg.batch_size, shuffle=True)
 
     optimizer = torch.optim.Adam([{"params": sfr.parameters()}], lr=cfg.lr)
-    criterion = torch.nn.CrossEntropyLoss(reduction="sum")
+
+    @torch.no_grad()
+    def map_pred_fn(x):
+        return torch.softmax(sfr.network(x.to(cfg.device)), dim=-1)
 
     for epoch in tqdm(list(range(cfg.n_epochs))):
         for X, y in train_loader:
@@ -214,32 +99,29 @@ def train(cfg: DictConfig):
             optimizer.step()
             wandb.log({"loss": loss})
         if epoch % cfg.logging_epoch_freq == 0:
-            tr_loss, tr_acc, tr_nll = evaluate(
-                network, train_loader, criterion, cfg.device
+            train_metrics = compute_metrics(
+                pred_fn=map_pred_fn,
+                ds_test=ds_train,
+                batch_size=cfg.batch_size,
+                device=cfg.device,
             )
-            wandb.log({"training/loss": tr_loss})
-            wandb.log({"training/nll": tr_nll})
-            wandb.log({"training/acc": tr_acc})
+            test_metrics = compute_metrics(
+                pred_fn=map_pred_fn,
+                ds_test=ds_test,
+                batch_size=cfg.batch_size,
+                device=cfg.device,
+            )
+            wandb.log(train_metrics)
+            wandb.log(test_metrics)
             wandb.log({"epoch": epoch})
 
     logger.info("Finished training")
 
-    state = {
-        "model": network.state_dict(),
-        "optimizer": optimizer.state_dict(),
-        "delta": cfg.prior.delta,
-    }
+    state = {"model": sfr.state_dict(), "optimizer": optimizer.state_dict()}
 
-    res_dir = os.path.join(run.dir, "./saved_models")
-    if not os.path.exists(res_dir):
-        os.makedirs(res_dir)
-    fname = (
-        "./"
-        + "_".join([cfg.dataset, cfg.model_name, str(cfg.random_seed)])
-        + f"_{cfg.prior.delta:.1e}.pt"
-    )
     logger.info("Saving model and optimiser etc...")
-    torch.save(state, os.path.join(res_dir, fname))
+    fname = "ckpt_dict.pt"
+    torch.save(state, os.path.join(run.dir, fname))
     logger.info("Finished saving model and optimiser etc")
 
 

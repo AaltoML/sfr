8d96fb2382b653fa838cdbf1c9cbd89c188b7353
Aidan Scannell <scannell.aidan@gmail.com>
2023-06-05 10:57:32 +0300

    initial commit

---
+++ experiments/sl/utils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import random
+from typing import Optional
+
+import numpy as np
+import src
+import torch
+
+
+def set_seed_everywhere(random_seed):
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+
+
+def init_SFR_with_gaussian_prior(
+    model: torch.nn.Module,
+    delta: float,
+    likelihood: src.likelihoods.Likelihood,
+    output_dim: int,
+    num_inducing: int = 30,
+    dual_batch_size: Optional[int] = None,
+    jitter: float = 1e-6,
+    device: str = "cpu",
+):
+    prior = src.priors.Gaussian(params=model.parameters, delta=delta)
+    return src.SFR(
+        network=model,
+        prior=prior,
+        likelihood=likelihood,
+        output_dim=output_dim,
+        num_inducing=num_inducing,
+        dual_batch_size=dual_batch_size,
+        jitter=jitter,
+        device=device,
+    )

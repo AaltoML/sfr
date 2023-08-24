9d646fd680d31f3280538c0671346bc1db054809
Aidan Scannell <scannell.aidan@gmail.com>
2023-08-24 14:42:32 +0300

    neatened for pual

--- experiments/sl/fast_updates.py
+++ experiments/sl/fast_updates.py
@@ -9,6 +9,7 @@ from typing import Optional
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+import src
 import hydra
 import numpy as np
 import pandas as pd
@@ -17,7 +18,7 @@ import wandb
 from experiments.sl.train import checkpoint
 from experiments.sl.utils import compute_metrics, EarlyStopper, set_seed_everywhere
 from omegaconf import DictConfig, OmegaConf
-from torch.utils.data import ConcatDataset, DataLoader
+from torch.utils.data import ConcatDataset, DataLoader, Dataset
 from tqdm import tqdm
 
 
@@ -35,6 +36,7 @@ class TableLogger:
             "ece": [],
             "prior_prec": [],
             "time": [],
+            "method": [],
         }
         self.tbl = wandb.Table(
             columns=[
@@ -47,6 +49,7 @@ class TableLogger:
                 "ece",
                 "prior_prec",
                 "time",
+                "method",
             ]
         )
 
@@ -57,6 +60,7 @@ class TableLogger:
         prior_prec: float,
         num_inducing: Optional[int] = None,
         time: float = None,
+        method: str = None,
     ):
         "Add NLL to data dict and wandb table"
         if isinstance(prior_prec, torch.Tensor):
@@ -70,6 +74,7 @@ class TableLogger:
         self.data["ece"].append(metrics["ece"])
         self.data["prior_prec"].append(prior_prec)
         self.data["time"].append(time)
+        self.data["method"].append(method)
         self.tbl.add_data(
             self.cfg.dataset.name,
             model_name,
@@ -80,8 +85,26 @@ class TableLogger:
             metrics["ece"],
             prior_prec,
             time,
+            method,
         )
         wandb.log({"Metrics": wandb.Table(data=pd.DataFrame(self.data))})
+
+
+def make_ds_double(ds: Dataset) -> Dataset:
+    try:
+        ds.data = ds.data.to(torch.double)
+        ds.targets = ds.targets.long()
+    except:
+        ds.dataset.data = ds.dataset.data.to(torch.double)
+        ds.dataset.targets = ds.dataset.targets.long()
+    return ds
+
+
+def make_data_loader_double(data_loader: DataLoader) -> DataLoader:
+    double_data_loader = copy.deepcopy(data_loader)
+    ds = double_data_loader.dataset
+    make_ds_double(ds)
+    return double_data_loader
 
 
 @hydra.main(version_base="1.3", config_path="./configs", config_name="fast_updates")
@@ -101,10 +124,7 @@ def main(cfg: DictConfig):
 
     torch.set_default_dtype(torch.float)
 
-    # # Load train/val/test data sets
-    # ds_train, ds_val, ds_test = hydra.utils.instantiate(
-    #     cfg.dataset, dir=os.path.join(get_original_cwd(), "data")
-    # )
+    # Load train/val/test/update data sets
     ds_train, ds_val, ds_test, ds_update = hydra.utils.instantiate(
         cfg.dataset,
         dir=os.path.join(get_original_cwd(), "data"),
@@ -119,6 +139,8 @@ def main(cfg: DictConfig):
     train_and_update_loader = DataLoader(
         ConcatDataset([ds_train, ds_update]), batch_size=cfg.batch_size, shuffle=True
     )
+    test_loader_double = make_data_loader_double(data_loader=test_loader)
+    update_loader_double = make_data_loader_double(data_loader=update_loader)
 
     # Init Weight and Biases
     cfg.output_dim = ds_train.output_dim
@@ -154,8 +176,6 @@ def main(cfg: DictConfig):
             cum_loss += loss
         return cum_loss
 
-    # Train
-    # sfr = train(cfg)  # Train the NN
     def train_loop(sfr, data_loader: DataLoader):
         optimizer = torch.optim.Adam([{"params": sfr.parameters()}], lr=cfg.lr)
 
@@ -214,165 +234,95 @@ def main(cfg: DictConfig):
                     break
         return sfr
 
-    start_time = time.time()
-    sfr = train_loop(sfr, data_loader=train_loader)
-    train_time = time.time() - start_time
+    def train_and_log(
+        sfr: src.SFR,
+        train_loader: DataLoader,
+        name: str,
+        inference_loader: Optional[DataLoader] = None,
+    ):
+        torch.set_default_dtype(torch.float)
 
-    # Make everything double for inference
-    torch.set_default_dtype(torch.double)
-    sfr.double()
-    sfr.eval()
+        # Train NN
+        sfr.float()
+        sfr.train()
+        start_time = time.time()
+        sfr = train_loop(sfr, data_loader=train_loader)
+        train_time = time.time() - start_time
 
-    # ds_train, ds_val, ds_test, ds_update = hydra.utils.instantiate(
-    #     cfg.dataset,
-    #     dir=os.path.join(get_original_cwd(), "data"),
-    #     double=cfg.double_inference,
-    #     train_update_split=cfg.train_update_split,
-    # )
-    # train_loader = DataLoader(ds_train, batch_size=cfg.batch_size, shuffle=True)
-    # val_loader = DataLoader(ds_val, batch_size=cfg.batch_size, shuffle=False)
-    # test_loader = DataLoader(ds_test, batch_size=cfg.batch_size, shuffle=True)
-    # update_loader = DataLoader(ds_update, batch_size=cfg.batch_size, shuffle=True)
-    def make_ds_double(ds):
-        try:
-            ds.data = ds.data.to(torch.double)
-            ds.targets = ds.targets.long()
-        except:
-            ds.dataset.data = ds.dataset.data.to(torch.double)
-            ds.dataset.targets = ds.dataset.targets.long()
-        return ds
+        # Make everything double for inference
+        torch.set_default_dtype(torch.double)
 
-    def make_data_loader_double(data_loader):
-        double_data_loader = copy.deepcopy(data_loader)
-        ds = double_data_loader.dataset
-        make_ds_double(ds)
-        return double_data_loader
+        # Log MAP before updates
+        sfr.double()
+        sfr.eval()
+        log_map_metrics(
+            sfr,
+            test_loader_double,
+            name=name,
+            table_logger=table_logger,
+            device=cfg.device,
+            time=train_time,
+        )
 
-    train_loader_double = make_data_loader_double(data_loader=train_loader)
-    val_loader_double = make_data_loader_double(data_loader=val_loader)
-    test_loader_double = make_data_loader_double(data_loader=test_loader)
-    update_loader_double = make_data_loader_double(data_loader=update_loader)
-    # train_and_update_loader_double = make_data_loader_double(
-    #     data_loader=train_and_update_loader
-    # )
+        # Fit SFR
+        logger.info("Fitting SFR...")
+        if inference_loader is None:
+            inference_loader = train_loader
+        start_time = time.time()
+        sfr.fit(train_loader=inference_loader)
+        inference_time = time.time() - start_time
+        logger.info("Finished fitting SFR")
 
-    # Log MAP before updates
-    log_map_metrics(
-        sfr,
-        test_loader_double,
-        name="train D1",
-        table_logger=table_logger,
-        device=cfg.device,
-        time=train_time,
-    )
+        # Log SFR
+        log_sfr_metrics(
+            sfr,
+            name=name,
+            test_loader=test_loader_double,
+            table_logger=table_logger,
+            device=cfg.device,
+            time=inference_time,
+        )
+        return sfr
 
-    # Fit SFR before updates
-    sfr.double()
-    sfr.eval()
-    logger.info("Fitting SFR...")
-    sfr.fit(train_loader=train_loader)
-    inference_time = time.time() - start_time
-    logger.info("Finished fitting SFR")
+    # Train on D1 and log
+    sfr = train_and_log(sfr, train_loader=train_loader, name="Train D1")
 
-    # Log SFR before updates
-    log_sfr_metrics(
-        sfr,
-        name="train D1",
-        test_loader=test_loader_double,
-        table_logger=table_logger,
-        device=cfg.device,
-        time=inference_time,
-    )
-
-    # TODO Do fast updates and log metrics
+    # Dual updates on D2 and log
     start_time = time.time()
     sfr.update_from_dataloader(data_loader=update_loader_double)
     update_inference_time = time.time() - start_time
     log_sfr_metrics(
         sfr,
-        name="train D1, update D2",
+        name="Train D1 -> Update D2",
         test_loader=test_loader_double,
         table_logger=table_logger,
         device=cfg.device,
         time=update_inference_time,
     )
 
-    # TODO Continue training on all data and log metrics
-    sfr.float()
-    sfr.train()
-    start_time = time.time()
-    sfr = train_loop(sfr, data_loader=train_and_update_loader)
-    update_train_time = time.time() - start_time
-    sfr.double()
-    sfr.eval()
-    log_map_metrics(
-        sfr,
-        test_loader_double,
-        name="continue train D1+D2",
-        table_logger=table_logger,
-        device=cfg.device,
-        time=update_train_time,
+    # Continue training on D1+D2 and log
+    sfr = train_and_log(
+        sfr, train_loader=train_and_update_loader, name="Train D1 -> Train D1+D2"
     )
 
-    # Fit SFR after updates
-    sfr.double()
-    sfr.eval()
-    logger.info("Fitting SFR...")
-    # sfr.fit(train_loader=train_and_update_loader_double)
-    start_time = time.time()
-    sfr.fit(train_loader=train_and_update_loader)
-    train_and_update_inference_time = time.time() - start_time
-    logger.info("Finished fitting SFR")
-
-    # Log SFR after updates
-    log_sfr_metrics(
-        sfr,
-        name="continue train D1+D2",
-        test_loader=test_loader_double,
-        table_logger=table_logger,
-        device=cfg.device,
-        time=train_and_update_inference_time,
-    )
-
-    # Retrain from scratch
+    # Train on D1+D2 (from scratch) and log
     network = hydra.utils.instantiate(cfg.network, ds_train=ds_train)
     sfr = hydra.utils.instantiate(cfg.sfr, model=network)
+    sfr = train_and_log(sfr, train_loader=train_and_update_loader, name="Train D1+D2")
+
+    # Continue training on just D2 and log
+    print("Continue training on just D2 and log")
+    torch.set_default_dtype(torch.float)
+    network = hydra.utils.instantiate(cfg.network, ds_train=ds_train)
     sfr.float()
-    sfr.train()
-    print(f"made sfr {sfr}")
-
-    start_time = time.time()
-    sfr = train_loop(sfr, data_loader=train_and_update_loader)
-    update_train_time = time.time() - start_time
-    sfr.double()
-    sfr.eval()
-    log_map_metrics(
-        sfr,
-        test_loader_double,
-        name="retrain D1+D2",
-        table_logger=table_logger,
-        device=cfg.device,
-        time=update_train_time,
-    )
-
-    # Fit SFR after retraining from scrath
-    sfr.double()
-    sfr.eval()
-    logger.info("Fitting SFR...")
-    start_time = time.time()
-    # sfr.fit(train_loader=train_and_update_loader_double)
-    sfr.fit(train_loader=train_and_update_loader)
-    train_and_update_inference_time = time.time() - start_time
-    logger.info("Finished fitting SFR")
-
-    # Log SFR after updates
-    log_sfr_metrics(
-        sfr,
-        name="retrain D1+D2",
-        test_loader=test_loader_double,
-        table_logger=table_logger,
-        device=cfg.device,
-        time=train_and_update_inference_time,
+    sfr.network.float()
+    network.load_state_dict(sfr.network.state_dict())  # copy weights and stuff
+    sfr_copy = hydra.utils.instantiate(cfg.sfr, model=network)
+    _ = train_and_log(
+        sfr_copy,
+        train_loader=update_loader,
+        name="Train D1 -> Train D2",
+        inference_loader=train_and_update_loader,
     )
 
     # # Log table on W&B and save latex table as .tex
@@ -402,11 +352,12 @@ def log_map_metrics(
         pred_fn=map_pred_fn, data_loader=test_loader, device=device
     )
     table_logger.add_data(
-        "NN MAP " + name,
+        "NN MAP",
         metrics=map_metrics,
         num_inducing=None,
         prior_prec=sfr.prior.delta,
         time=time,
+        method=name,
     )
     logger.info(f"map_metrics: {map_metrics}")
 
@@ -445,11 +396,12 @@ def log_sfr_metrics(
         device=device,
     )
     table_logger.add_data(
-        "SFR (GP) " + name,
+        "SFR (GP)",
         metrics=gp_metrics,
         num_inducing=sfr.num_inducing,
         prior_prec=sfr.prior.delta,
         time=time,
+        method=name,
     )
 
 
